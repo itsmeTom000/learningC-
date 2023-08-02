280f6e72dc11f0819a6559378f098007d2cd9340
itsme-BIOMETRIX <126332808+itsme-BIOMETRIX@users.noreply.github.com>
2023-08-02 18:49:36 +0530

    new code

--- Source.cpp
+++ Source.cpp
@@ -5,5 +5,13 @@
 int main() {
     std::cout << "Hello World!";
     std::cout << " helllo ";
+        int number;
+
+        cout << "Enter an integer: ";
+        cin >> number;
+
+        cout << "You entered " << number;
+        return 0;
+    }
     return 0;
 }
